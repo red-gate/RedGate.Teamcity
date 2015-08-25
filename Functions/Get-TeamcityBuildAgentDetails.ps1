026b71b5f627d182c2451f8d37e8c1225a2a9616
nico.vanelslande <nico.vanelslande@red-gate.com>
2015-08-25 12:42:04 +0100

    Improved Get-TeamcityBuildAgentDetails to also retrieve the machine hostname

--- Functions/Get-TeamcityBuildAgentDetails.ps1
+++ Functions/Get-TeamcityBuildAgentDetails.ps1
@@ -8,8 +8,22 @@ function Get-TeamcityBuildAgentDetails
 
 		process {
 
-			([xml](Invoke-WebRequest "$TeamcityServer/httpAuth/app/rest/agents/id:$Id" -Credential $Credential).Content).agent
+			$agentData = ([xml](Invoke-WebRequest "$TeamcityServer/httpAuth/app/rest/agents/id:$Id" -Credential $Credential).Content).agent
+
+			[pscustomobject] @{
+				Id = $agentData.id
+				Name = $agentData.name
+				Typeid = $agentData.typeid
+				Connected = $agentData.connected
+				Enabled = $agentData.enabled
+				Authorized = $agentData.authorized
+				Uptodate = $agentData.uptodate
+				Ip = $agentData.ip
+				Properties = $agentData.properties.property
+				Pool = $agentData.pool
+				Hostname = [System.Net.Dns]::gethostentry($agentData.ip).hostname
+			}
 
 		}
-		
+
 }
