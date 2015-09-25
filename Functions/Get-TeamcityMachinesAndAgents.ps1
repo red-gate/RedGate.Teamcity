d9c62d7f52bee620c306c58181c66162838d9417
nico.vanelslande <nico.vanelslande@red-gate.com>
2015-09-25 16:15:05 +0100

    remove code duplication...
    Get-TeamcityBuildAgentDetails gives us the agent machine name.

--- Functions/Get-TeamcityMachinesAndAgents.ps1
+++ Functions/Get-TeamcityMachinesAndAgents.ps1
@@ -5,27 +5,6 @@ function Get-TeamcityMachinesAndAgents
 		[string] $AgentNamePattern = '*'
 	)
 
-	$agents = @(Get-TeamcityBuildAgents -NamePattern $AgentNamePattern)
-
-	$i = 0
-	$agents | ForEach {
-
-		$i++
-		$local:ProgressPreference = 'Continue'
-		Write-Progress -Activity "Querying Teamcity..." -PercentComplete ($i / $agents.count * 100)
-		$local:ProgressPreference = 'SilentlyContinue'
-
-		$agentDetails = ([xml](Invoke-WebRequest "$TeamcityServer$($_.href)" -Credential $Credential).Content).agent
-		$machineName = $agentDetails.properties.property | where name -eq 'env.COMPUTERNAME' | select -ExpandProperty value
-		$pool = $agentDetails.pool.name
-
-		[PsCustomObject] @{
-			Id = $agentDetails.id
-			Name = $agentDetails.name.ToUpper()
-			Machine = $machineName
-			Pool = $pool.ToUpper()
-		}
-
-	}
+	Get-TeamcityBuildAgents -NamePattern $AgentNamePattern | Get-TeamcityBuildAgentDetails		
 
 }
