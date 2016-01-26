46c4cfce58f901f73f89a364fa2eecf8b4c22a4e
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-01-26 10:36:17 +0000

    Get-TeamcityBuildConfigs now has a -Locator parameter for additional filtering.

--- Functions/Get-TeamcityBuildConfigs.ps1
+++ Functions/Get-TeamcityBuildConfigs.ps1
@@ -1,8 +1,22 @@
+<#
+.SYNOPSIS
+Retrieve a list of build configuration from a Teamcity server.
+#>
 function Get-TeamcityBuildConfigs
 {
-	[CmdletBinding()]
-  param()
+    [CmdletBinding()]
+    param(
+        # Optional, a '<buildTypeLocator>' as defined in https://confluence.jetbrains.com/display/TCD9/REST+API#RESTAPI-BuildConfigurationLocator
+        # to filter the list of build configs returned.
+        # Example: -Locator '?project=project1'
+        # Example: -Locator '?paused=true'
+        [string] $Locator
+    )
 
-	([xml](Invoke-WebRequest "$TeamcityServer/httpAuth/app/rest/buildTypes" -Credential $Credential).Content).buildTypes.buildType
+    if( $Locator -and !$Locator.StartsWith('?') ) {
+        throw "Invalid Locator: '$Locator'. -Locator is expected to start with '?'"
+    }
+
+    ([xml](Invoke-WebRequest "$TeamcityServer/httpAuth/app/rest/buildTypes/$Locator" -Credential $Credential).Content).buildTypes.buildType
 
 }
