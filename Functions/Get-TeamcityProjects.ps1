732f759334032b063e3c7c29ce1a7e623a66511c
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-01-26 10:32:10 +0000

    Get-TeamcityProjects now as an -Archived parameter.
    + help

--- Functions/Get-TeamcityProjects.ps1
+++ Functions/Get-TeamcityProjects.ps1
@@ -1,8 +1,24 @@
+<#
+.SYNOPSIS
+Retrieve a list of projects from a Teamcity server.
+#>
 function Get-TeamcityProjects
 {
-	[CmdletBinding()]
-  param()
+    [CmdletBinding()]
+    param(
+        # If set to 'true', only retrieves projects that are archived
+        # If set to 'false'm only retrieves projects that are not archived
+        # Default: If set to $null, retrieves all projects regardless of their archived status
+        [ValidateSet($null, 'true', 'false')]
+        [string] $Archived = $null
+    )
 
-	([xml](Invoke-WebRequest "$TeamcityServer/httpAuth/app/rest/projects" -Credential $Credential).Content).projects.project
+    $projects = ([xml](Invoke-WebRequest "$TeamcityServer/httpAuth/app/rest/projects" -Credential $Credential).Content).projects.project
+
+    if($Archived) {
+        $projects | where archived -eq $Archived
+    } else {
+        $projects
+    }
 
 }
